dae74b38ca043cdff4a5f34af1f18c31f81946ab
Lior Keren <liorkgow@gmail.com>
2023-04-12 18:30:52 +0300

    Creating an index page for the application

---
+++ react_app/src/pages/IndexPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const IndexPage = () => (
+  <>
+    <h1>Welcome to EmoLens</h1>
+    <p>
+      This is the project of Guy & Lior
+    </p>
+  </>
+);
+
+export default IndexPage;
