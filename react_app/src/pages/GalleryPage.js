4e1e0ff014c4c864de2d86d8da0e8a5752850a1c
Lior Keren <liorkgow@gmail.com>
2023-07-29 13:10:48 +0300

    Fetch & Present Data From the Server
    URL of the API is :
    https://emo-lens-default-rtdb.europe-west1.firebasedatabase.app/images.json

--- react_app/src/pages/GalleryPage.js
+++ react_app/src/pages/GalleryPage.js
@@ -1,13 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 import { Typography, Container } from "@mui/material";
 
 import NavBar from "../components/NavBar";
 import Gallery from "../components/Gallery";
 import Footer from "../components/Footer";
 
-import { cards } from "../resources/Mock Data/mockData";
-
 const GalleryPage = () => {
+  const [cards, setCards] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = async (setCards) => {
+    const response = await Axios.get(
+      "https://emo-lens-default-rtdb.europe-west1.firebasedatabase.app/images.json"
+    );
+    
+    setCards(formatData(response.data));
+
+    setIsLoading(false);
+
+    const numOfCards = Object.keys(response.data).length;
+    console.log(`Fetched ${numOfCards} cards from the server`);
+  };
+
+  const formatData = (data) => {
+
+    const formattedData = [];
+
+    for (let key in data) {
+      
+      // emotion:
+      const emotion = data[key].emotion;
+      const emotionCapitalized = emotion.charAt(0).toUpperCase() + emotion.slice(1);
+
+      // gender: 
+      const gender = data[key].gender;
+      const genderCapitalized = gender.charAt(0).toUpperCase() + gender.slice(1);
+
+      // date:
+      const timestamp = parseInt(data[key].timestamp);
+      const date = new Date(timestamp);
+
+      formattedData.push({
+        id: key,
+        title: `${emotionCapitalized} ${genderCapitalized}`,
+        description: `Age: ${data[key].age}, Date: ${date}`,
+        image: data[key].url,
+      });
+    }
+
+    return formattedData;
+  };
+
+  // fetch data from the server when the component is mounted
+  useEffect(() => {
+    fetchData(setCards);
+  }, []);
+
   return (
     <div className="gallery-page">
       <NavBar />
@@ -32,7 +81,7 @@ const GalleryPage = () => {
         </Typography>
       </Container>
 
-      <Gallery cards={cards} />
+      <Gallery cards={cards} isLoading={isLoading} />
 
       <Footer />
     </div>
